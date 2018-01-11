c3ba7fdae38991e1b045fb511c3b883b398e66b8
Vidushan <kvidushan@hotmail.com>
2018-01-10 23:03:38 -0800

    Console logging user input

---
+++ popup.js
@@ -0,0 +1,13 @@
+function inputChange(e) {
+  if(e.keyCode === 13){
+    e.preventDefault();
+    const busID = document.getElementById("busID").value;
+    console.log(busID);
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = `http://api.translink.ca/RTTIAPI/V1/stops/55612?apikey=GMPEN4nbnZxrUBYQYkVh`;
+    fetch(proxyurl + url)
+      .then(response => response.text())
+      .then(contents => console.log(contents))
+      .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+  }
+}
